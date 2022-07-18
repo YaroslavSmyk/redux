2b342d5221a4fe8177f72f6ccf2781939b948b5f
YaroslavSmyk <YaroslavSmyk@gmail.com>
2022-07-18 22:09:20 +0300

    v6

--- lesson3/task1/src/users.actions.js
+++ lesson3/task1/src/users.actions.js
@@ -1,24 +1,31 @@
-export const ADD_USER = 'USER/ADD';
-export const DELETE_USER = 'USER/DELETE';
+export const ADD_USER = 'USER/ADD_USER';
+export const DELETE_USER = 'USER/DELETE_USER';
 export const UPDATE_USER = 'USER/UPDATE_USER';
 
 export const addUser = (userData) => {
   return {
     type: ADD_USER,
-    payload: { userData },
+    payload: {
+      userData,
+    },
   };
 };
 
 export const deleteUser = (userId) => {
   return {
     type: DELETE_USER,
-    payload: { userId },
+    payload: {
+      userId,
+    },
   };
 };
 
-export const updateUser = (userData, userId) => {
+export const updateUser = (userId, userData) => {
   return {
     type: UPDATE_USER,
-    payload: { userData, userId },
+    payload: {
+      userId,
+      userData,
+    },
   };
 };
