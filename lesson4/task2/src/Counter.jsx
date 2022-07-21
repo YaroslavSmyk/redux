38c22cb98b8932b85def776cd7f91bcfb6ceb9b8
YaroslavSmyk <YaroslavSmyk@gmail.com>
2022-07-21 21:51:10 +0300

    v1

---
+++ lesson4/task2/src/Counter.jsx
@@ -0,0 +1,43 @@
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import { increment, decrement, reset } from './counter/counter.actions';
+
+// const Counter = ({counter, increment, decrement, reset}) => {
+//   return (
+// {/* <div class="users">
+//   <button class="users__create-btn">Create user</button>
+//   <ul class="users__list">
+//     <li class="users__list-item">
+//       <span>User # 232286</span>
+//       <button class="users__delete-btn">+</button>
+//     </li>
+//     <li class="users__list-item">
+//       <span>User # 551949</span>
+//       <button class="users__delete-btn">+</button>
+//     </li>
+//     <li class="users__list-item">
+//       <span>User # 328468</span>
+//       <button class="users__delete-btn">+</button>
+//     </li>
+//   </ul>
+// </div> */}
+//   );
+// };
+
+// const mapState = (state) => {
+//   return {
+//     counter: state,
+//   };
+// };
+
+// const mapDispatch = (dispatch) => {
+//   return {
+//     increment: () => dispatch(increment()),
+//     decrement: () => dispatch(decrement()),
+//     reset: () => dispatch(reset()),
+//   };
+// };
+
+// const connector = connect(mapState, mapDispatch);
+
+// export default connector(Counter);
