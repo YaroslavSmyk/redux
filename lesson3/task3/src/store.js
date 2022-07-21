53eb9a4fec9a3d3ffdcc5ef560bd52ee4bfafac2
YaroslavSmyk <YaroslavSmyk@gmail.com>
2022-07-21 11:36:52 +0300

    м2

--- lesson3/task3/src/store.js
+++ lesson3/task3/src/store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from './redux';
+import { createStore, combineReducers } from 'redux';
 import { userReduser } from './user.reducer';
 import { cartReducer } from './cart.reducer';
 import { languageReducer } from './language.reducer';
