ead69d35bbfedc4386b18e8e5ef16f2479fceeba
YaroslavSmyk <YaroslavSmyk@gmail.com>
2022-08-02 21:40:07 +0300

    v5

--- lesson6/task3/src/weather/weather.actions.js
+++ lesson6/task3/src/weather/weather.actions.js
@@ -1,6 +1,6 @@
 import { fetchWeatherData } from './weather.gateway';
 
-export const ADD_WEATHER_DATA = 'WEATHER_DATA';
+export const ADD_WEATHER_DATA = 'ADD_WEATHER_DATA';
 
 export const weatherDataReceived = (weatherData) => {
   return {
